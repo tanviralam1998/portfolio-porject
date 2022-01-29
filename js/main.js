e4eabd5e8cd95488781a16278d12bea9332981ba
tanviralam1998 <tanvir.ctg1998@gmail.com>
2022-01-29 20:37:19 +0600

    update project

---
+++ js/main.js
@@ -0,0 +1,11 @@
+(function($){
+    'use strict';
+    
+//slide toogle function for nav
+    $('.menu-icon').on('click', function(){
+        $('.main-menu').slideToggle();
+    });
+
+
+
+}) (jQuery);;
